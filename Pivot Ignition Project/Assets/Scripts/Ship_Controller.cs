59357725d140bf906ef2bb376ac3f43711ad8c4b
Carlo di Paolo <carlodipaolo94@gmail.com>
2021-01-06 19:39:48 +0100

    Ship Controller script Log testing

---
+++ Pivot Ignition Project/Assets/Scripts/Ship_Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Controller : MonoBehaviour
+{
+    private bool leftTurn = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    private static void PlayerInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("THRUSTERS ENGAGED");
+        }
+
+
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            print("!!!Conflicting Rotation!!!");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        } 
+    }
+}
